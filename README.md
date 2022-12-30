5f879198d7b34cdf823fc8530efcb0770a7a7caf
Sean Johnson <johnson-sean@outlook.com>
2022-12-29 22:47:33 -0500

    added readme

---
+++ README.md
@@ -0,0 +1,17 @@
+# Data Wrangling of Income and Wealth Statistical Data
+
+The objective for this project is to extract data from an xlsx file with many tabs and generate a tidier data format for use.
+
+## Tabular Examples of Extracted Data
+
+### Entire Country
+
+![](static-files/table_USA.png)
+
+### State Level
+
+![](static-files/table_TN.png)
+
+#### Citation
+>U.S. Department of Economic Research Service. Income and Wealth Statistics, December 1, 2022.
+https://www.ers.usda.gov/data-products/farm-income-and-wealth-statistics/data-files-u-s-and-state-level-farm-income-and-wealth-statistics/
--- r/Data-Wrangling-Income-Wealth-Stats.R
+++ r/Data-Wrangling-Income-Wealth-Stats.R
@@ -1,7 +1,7 @@
 library(dplyr)
 library(purrr)
 
-#usda
+# ERS
 # https://www.ers.usda.gov/data-products/farm-income-and-wealth-statistics/data-files-u-s-and-state-level-farm-income-and-wealth-statistics/
 
 # data was collected from the following site
@@ -13,10 +13,10 @@ tabs <- readxl::excel_sheets(path)
 tabs <- tabs[2:length(tabs)]
 
 #farm income and wealth statistics
-fiws <- lapply(tabs,function(x) readxl::read_excel(path, sheet = x, skip = 0, col_names = F))%>%
+iws <- lapply(tabs,function(x) readxl::read_excel(path, sheet = x, skip = 0, col_names = F))%>%
   set_names(nm=tabs)
 
-test <- map(fiws, .f = ~.x %>% janitor::remove_empty(which = c("cols","rows"))%>%
+iws <- map(iws, .f = ~.x %>% janitor::remove_empty(which = c("cols","rows"))%>%
               filter(!(is.na(...1)| stringr::str_detect(string= ...1, pattern = "Footnotes") |
                          stringr::str_detect(string= ...1,pattern = "USDA/ERS Farm Income and Wealth Statistics")|
                          stringr::str_detect(string= ...1,pattern = "Value added to the U.S. economy by the agricultural sector")))%>%
@@ -27,16 +27,26 @@ test <- map(fiws, .f = ~.x %>% janitor::remove_empty(which = c("cols","rows"))%>
                                       (n==1 & Field=="Inventory adjustment")~"Inventory adjustment for Crops",
                                       (n==2 & Field=="Home consumption")~"Home consumption for Animals",
                                       (n==2 & Field=="Inventory adjustment")~"Inventory adjustment for Animals",
+                                     Field == "Net farm income" ~ "Net Income",
                                       TRUE ~ Field))%>%
               select(-n)%>%
               tidyr::pivot_longer(!Field, names_to = "Year", values_to = "Values"))%>%
               bind_rows(.id = "Table")%>%
-              mutate(Values = as.numeric(Values) * 1000, #data was origanally represented in thousands of dollars
+              mutate(Values = as.numeric(Values) * 1000, #data was originally represented in thousands of dollars
                      Type = substr(Year,5,6),
                      Year = substr(Year,1,4),
                      Type = case_when(Type == "F"~ "Forecast",
                                       TRUE~ "Historical"))
-  
+# Tabular Examples of Data ----
 
+iws%>%
+  filter(Table == "United States",
+         Field == "Electricity")%>%
+  Thematic::tabGT(table_title = "Historical and Predicted Values for Electricity Cost",
+                  table_subtitle = "Data pertains to the entire United States")
 
-
+iws%>%
+  filter(Table == "Tennessee",
+         Field == "Net Income")%>%
+  Thematic::tabGT(table_title = "Historical Values for Net Income",
+                  table_subtitle = "Data pertains only to Tenessee")
